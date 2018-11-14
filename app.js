669d0c87ef665404f4b1f684fac46e052de5f97d
Jay VanOrd <jvanord@indasysllc.com>
2018-11-14 11:46:07 -0500

    comment saver prototyped

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const config = {
 	account: 'mediaservicesgroup',
 	projectId: '10815255',
+	commentLimit: 3, // null for unlimited
 	commentDelay: '2000',  // don't want to look like a DOS attack
 	sessionCookie:
 		'twisted_token=1c1e8b794c2f3bb489e4bd4f67e9c370c12a; _jsuid=1411205140; session_token=fa7cf68e2817851fe48d; _basecamp_session_v2=BAh7CkkiD3Nlc3Npb25faWQGOgZFRiIlNjMwNmNhOGExMDVlMDYyNWMwZjdhNzE5ZmU0MTA2ZWVJIgx1c2VyX2lkBjsARmkDNJRuSSIQaWRlbnRpdHlfaWQGOwBGaQN5Ai1JIh1tZXNhdXJlX3BhZ2VfcGVyZm9ybWFuY2UGOwBGRkkiEF9jc3JmX3Rva2VuBjsARkkiMTdBL0huTzR5YnBBbXhCOVN4cFZtbFFHeWZDMklKUi9oZG1ERC84WnpTb0k9BjsARg%3D%3D--4016130c70e27f762461fc7a38d4cfde568e5264; flashVersion='
@@ -56,7 +57,6 @@ const parseProjectInfo = $ => {
 			let $item = $(e);
 			let itemId = $item.attr('record');
 			let $title = $item.find('#item_wrap_' + itemId);
-			//list_29629224_item_223348400_text
 			return {
 				id: itemId,
 				title: $title.text().replace(/(^\s+|[\t\r\n]|\s+$)/g, '')
@@ -66,7 +66,7 @@ const parseProjectInfo = $ => {
 	return project;
 };
 
-const saveProject = project => {
+const saveProject = async project => {
 	if (!project) return console.error('Project Null or Undefined', project);
 	let fileName = 'Project_' + project.id + '_' + project.name.replace(/\s/g, '_') + '.json';
 	fs.writeFile(fileName, JSON.stringify(project, null, 1), err => {
@@ -75,11 +75,34 @@ const saveProject = project => {
 	});
 };
 
-async function getComments(){
-	console.log('Waiting 5 seconds as if we\'re getting comments.');
-	await sleep(5000);
-	console.log('Waited those 5 seconds.');
-}
+let commentsLoaded = 0;
+const getComments = async project => {
+	project.lists.forEach(list => {
+		list.todos.forEach(async todo => {
+			if (config.commentLimit && commentsLoaded >= config.commentLimit) return;
+			requestComments(project.id, todo.id)
+				.then($ => {
+					let $comments = $('#comments');
+					if ($comments.children().length < 1) return;
+					commentsLoaded++;
+					saveComments(project.name, list.name, todo, $comments.html());
+				})
+				.catch(onRequestError);
+			if (config.commentDelay) await sleep(config.commentDelay);
+		});
+	});
+};
+const getTodoComments = async (project, list, todo) => {
+	requestComments(project.id, todo.id)
+		.then($ => {
+			let $comments = $('#comments');
+			if ($comments.children().length < 1) return;
+			commentsLoaded++;
+			saveComments(project.name, list.name, todo, $comments.html());
+		})
+		.catch(onRequestError);
+	if (config.commentDelay) await sleep(config.commentDelay);
+};
 
 const requestComments = (projectId, todoId) =>
 	requestPromise({
@@ -91,6 +114,15 @@ const requestComments = (projectId, todoId) =>
 		transform: body => cheerio.load(body)
 	});
 
+const saveComments = async (projectName, listName, todo, commentHtml) => {
+	if (!commentHtml) return console.error('Comment Null or Undefined', projectName, listName, todo, commentHtml);
+	let fileName = 'Todo_' + todo.id + '_' + projectName.replace(/\s/g, '_') + '_' + listName.replace(/\s/g, '_') + '_' + todo.title.replace(/\s/g, '_') + '.txt';
+	fs.writeFile(fileName, commentHtml, err => {
+		if (err) return console.error(err);
+		console.log('Comment Saved at ' + fileName);
+	});
+};
+
 // Execute
 console.log('Running ...');
 requestProject(config.projectId)
@@ -99,7 +131,8 @@ requestProject(config.projectId)
 		let project = parseProjectInfo($);
 		console.info(project.lists.length + ' Lists Found');
 		saveProject(project);
-		getComments(project);
+		//getComments(project);
+		getTodoComments(project, project.lists[1], project.lists[1].todos[0]);
 		//console.log('Results', project);
 	})
 	.catch(onRequestError);
