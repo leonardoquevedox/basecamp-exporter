1d2a0e582d5f3b6e3c9e44f64f62fff5756e5207
Jay VanOrd <jvanord@indasysllc.com>
2018-11-14 09:42:34 -0500

    refactored

--- app.js
+++ app.js
@@ -8,9 +8,22 @@ const config = {
 		'twisted_token=1c1e8b794c2f3bb489e4bd4f67e9c370c12a; _jsuid=1411205140; session_token=fa7cf68e2817851fe48d; _basecamp_session_v2=BAh7CkkiD3Nlc3Npb25faWQGOgZFRiIlNjMwNmNhOGExMDVlMDYyNWMwZjdhNzE5ZmU0MTA2ZWVJIgx1c2VyX2lkBjsARmkDNJRuSSIQaWRlbnRpdHlfaWQGOwBGaQN5Ai1JIh1tZXNhdXJlX3BhZ2VfcGVyZm9ybWFuY2UGOwBGRkkiEF9jc3JmX3Rva2VuBjsARkkiMTdBL0huTzR5YnBBbXhCOVN4cFZtbFFHeWZDMklKUi9oZG1ERC84WnpTb0k9BjsARg%3D%3D--4016130c70e27f762461fc7a38d4cfde568e5264; flashVersion='
 };
 
-const onRequestComplete = $ => {
-	console.info('Request Succeed');
-	const project = {
+const onRequestError = error => {
+	console.error(error);
+};
+
+const requestProject = projectId => 
+	requestPromise({
+		uri: 'https://' + config.account + '.basecamphq.com/projects/' + projectId + '/todo_lists',
+		headers: {
+			'User-Agent': 'Request-Promise',
+			Cookie: config.sessionCookie
+		},
+		transform: body => cheerio.load(body)
+	});
+
+const parseProjectInfo = $ => {
+	let project = {
 		id: config.projectId,
 		name: $('#Header h1').clone().children().remove().end().text().replace(/(^\s+|[\t\r\n]|\s+$)/g,''),
 		lists: $('.list_wrapper').map((i, e) => {
@@ -21,20 +34,23 @@ const onRequestComplete = $ => {
 			};
 		}).get()
 	};
-	console.log('Results', project);
+	return project;
 };
 
-const onRequestError = error => {
-	console.error(error);
-};
-
-requestPromise({
-	uri: 'https://' + config.account + '.basecamphq.com/projects/' + config.projectId + '/todo_lists',
+const requestComments = (projectId, todoId) =>	requestPromise({
+	uri: 'https://' + config.account + '.basecamphq.com/projects/' + projectId + '/todo_items/' + todoId + '/comments',
 	headers: {
 		'User-Agent': 'Request-Promise',
 		Cookie: config.sessionCookie
 	},
 	transform: body => cheerio.load(body)
-})
-	.then(onRequestComplete)
+});
+
+// Execute
+requestProject(config.projectId)
+	.then($ => {
+		console.info('Request Succeed');
+		let project = parseProjectInfo($);
+		console.log('Results', project);
+	})
 	.catch(onRequestError);
