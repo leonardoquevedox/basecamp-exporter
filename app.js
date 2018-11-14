ba20608d2753b7772b65c6afd7769b49b30cc38f
Jay VanOrd <jvanord@indasysllc.com>
2018-11-14 09:29:10 -0500

    parsed all lists

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 const requestPromise = require('request-promise');
 const cheerio = require('cheerio');
 
-const requestOptions = {
-	uri: 'https://mediaservicesgroup.basecamphq.com/projects/10815255-plm/todo_lists',
+const config = {
+	account: 'mediaservicesgroup',
+	projectId: '10815255',
+	sessionCookie:
+		'twisted_token=1c1e8b794c2f3bb489e4bd4f67e9c370c12a; _jsuid=1411205140; session_token=fa7cf68e2817851fe48d; _basecamp_session_v2=BAh7CkkiD3Nlc3Npb25faWQGOgZFRiIlNjMwNmNhOGExMDVlMDYyNWMwZjdhNzE5ZmU0MTA2ZWVJIgx1c2VyX2lkBjsARmkDNJRuSSIQaWRlbnRpdHlfaWQGOwBGaQN5Ai1JIh1tZXNhdXJlX3BhZ2VfcGVyZm9ybWFuY2UGOwBGRkkiEF9jc3JmX3Rva2VuBjsARkkiMTdBL0huTzR5YnBBbXhCOVN4cFZtbFFHeWZDMklKUi9oZG1ERC84WnpTb0k9BjsARg%3D%3D--4016130c70e27f762461fc7a38d4cfde568e5264; flashVersion='
+};
+
+const onRequestComplete = $ => {
+	console.info('Request Succeed');
+	const project = {
+		id: config.projectId,
+		name: $('#Header h1').clone().children().remove().end().text().replace(/(^\s+|[\t\r\n]|\s+$)/g,''),
+		lists: $('.list_wrapper').map((i, e) => {
+			let $wrapper = $(e);
+			return {
+				id: $wrapper.attr('record'), 
+				name: $wrapper.find('h2 a').text().replace(/(^\s+|[\t\r\n]|\s+$)/g,'')
+			};
+		}).get()
+	};
+	console.log('Results', project);
+};
+
+const onRequestError = error => {
+	console.error(error);
+};
+
+requestPromise({
+	uri: 'https://' + config.account + '.basecamphq.com/projects/' + config.projectId + '/todo_lists',
 	headers: {
 		'User-Agent': 'Request-Promise',
-		'Cookie' : 'twisted_token=1c1e8b794c2f3bb489e4bd4f67e9c370c12a; _jsuid=1411205140; session_token=fa7cf68e2817851fe48d; _basecamp_session_v2=BAh7CkkiD3Nlc3Npb25faWQGOgZFRiIlNjMwNmNhOGExMDVlMDYyNWMwZjdhNzE5ZmU0MTA2ZWVJIgx1c2VyX2lkBjsARmkDNJRuSSIQaWRlbnRpdHlfaWQGOwBGaQN5Ai1JIh1tZXNhdXJlX3BhZ2VfcGVyZm9ybWFuY2UGOwBGRkkiEF9jc3JmX3Rva2VuBjsARkkiMTdBL0huTzR5YnBBbXhCOVN4cFZtbFFHeWZDMklKUi9oZG1ERC84WnpTb0k9BjsARg%3D%3D--4016130c70e27f762461fc7a38d4cfde568e5264; flashVersion='
+		Cookie: config.sessionCookie
 	},
-	transform: (body) => cheerio.load(body)
-};
-
-const onRequestComplete = ($) => { 
-	console.info('Request Succeed at ' + requestOptions.uri); 
-	const results = { lists: [] };
-	$('.list_wrapper').each((i, e) => {
-		let $wrapper = $(e);
-		let listId = $wrapper.attr('record');
-		results.lists.push({ id: listId, element: '#' + $wrapper.attr('id') + '.' + $wrapper.attr('class') });
-		//console.log('Found', $wrapper.html());
-	});
-	console.log('Results', results);
-};
-
-const onRequestError = (error) => { console.error(error); };
-
-requestPromise(requestOptions)
+	transform: body => cheerio.load(body)
+})
 	.then(onRequestComplete)
 	.catch(onRequestError);
